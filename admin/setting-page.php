0454a60ea8b7dd11c817a8f74d95908d429c8eb6
iranimij <iranimij@gmail.com>
2019-12-15 11:45:39 +0330

    add plugin detail

--- admin/setting-page.php
+++ admin/setting-page.php
@@ -31,37 +31,37 @@
                             <tr valign="top">
                                 <th scope="row">
                                     <label for="base_url">
-                                        <?php _e('Base URL:', 'auto-upload-images'); ?>
+                                        <?php _e('Base URL:', 'automatic-upload-images'); ?>
                                     </label>
                                 </th>
                                 <td>
                                     <input type="text" name="base_url" value="<?php echo esc_url(self::getOption('base_url')); ?>" class="regular-text" dir="ltr" />
-                                    <p class="description"><?php _e('If you need to choose a new base URL for the images that will be automatically uploaded. Ex:', 'auto-upload-images'); ?> <code>https://iranimij.com</code>, , <code>/</code></p>
+                                    <p class="description"><?php _e('If you need to choose a new base URL for the images that will be automatically uploaded. Ex:', 'automatic-upload-images'); ?> <code>https://iranimij.com</code>, , <code>/</code></p>
                                 </td>
                             </tr>
                             <tr valign="top">
                                 <th scope="row">
                                     <label for="image_name">
-                                        <?php _e('Image Name:', 'auto-upload-images'); ?>
+                                        <?php _e('Image Name:', 'automatic-upload-images'); ?>
                                     </label>
                                 </th>
                                 <td>
                                     <input type="text" name="image_name" value="<?php echo esc_html(self::getOption('image_name')); ?>" class="regular-text" dir="ltr" />
                                     <p class="description">
-                                        <?php printf(__('Choose a custom filename for the new images will be uploaded. You can also use these shortcodes %s.', 'auto-upload-images'), '<code dir="ltr">%filename%</code>, <code dir="ltr">%image_alt%</code>, <code dir="ltr">%url%</code>, <code dir="ltr">%date%</code>, <code dir="ltr">%year%</code>, <code dir="ltr">%month%</code>, <code dir="ltr">%day%</code>, <code dir="ltr">%random%</code>, <code dir="ltr">%timestamp%</code>, <code dir="ltr">%postname%</code>, <code dir="ltr">%post_id%</code>') ?>
+                                        <?php printf(__('Choose a custom filename for the new images will be uploaded. You can also use these shortcodes %s.', 'automatic-upload-images'), '<code dir="ltr">%filename%</code>, <code dir="ltr">%image_alt%</code>, <code dir="ltr">%url%</code>, <code dir="ltr">%date%</code>, <code dir="ltr">%year%</code>, <code dir="ltr">%month%</code>, <code dir="ltr">%day%</code>, <code dir="ltr">%random%</code>, <code dir="ltr">%timestamp%</code>, <code dir="ltr">%postname%</code>, <code dir="ltr">%post_id%</code>') ?>
                                     </p>
                                 </td>
                             </tr>
                             <tr valign="top">
                                 <th scope="row">
                                     <label for="alt_name">
-                                        <?php _e('Alt Name:', 'auto-upload-images'); ?>
+                                        <?php _e('Alt Name:', 'automatic-upload-images'); ?>
                                     </label>
                                 </th>
                                 <td>
                                     <input type="text" name="alt_name" value="<?php echo esc_html(self::getOption('alt_name')); ?>" class="regular-text" dir="ltr" />
                                     <p class="description">
-                                        <?php printf(__('Choose a custom alt name for the new images will be uploaded. You can also use these shortcodes %s.', 'auto-upload-images'), '<code dir="ltr">%filename%</code>, <code dir="ltr">%image_alt%</code>, <code dir="ltr">%url%</code>, <code dir="ltr">%date%</code>, <code dir="ltr">%year%</code>, <code dir="ltr">%month%</code>, <code dir="ltr">%day%</code>, <code dir="ltr">%random%</code>, <code dir="ltr">%timestamp%</code>, <code dir="ltr">%postname%</code>, <code dir="ltr">%post_id%</code>') ?>
+                                        <?php printf(__('Choose a custom alt name for the new images will be uploaded. You can also use these shortcodes %s.', 'automatic-upload-images'), '<code dir="ltr">%filename%</code>, <code dir="ltr">%image_alt%</code>, <code dir="ltr">%url%</code>, <code dir="ltr">%date%</code>, <code dir="ltr">%year%</code>, <code dir="ltr">%month%</code>, <code dir="ltr">%day%</code>, <code dir="ltr">%random%</code>, <code dir="ltr">%timestamp%</code>, <code dir="ltr">%postname%</code>, <code dir="ltr">%post_id%</code>') ?>
                                     </p>
                                 </td>
                             </tr>
@@ -69,19 +69,19 @@
                                 <?php $editor_supports = wp_image_editor_supports(); ?>
                                 <tr valign="top" <?= !$editor_supports ? 'style="background-color:#dedede;color:#6d6d6d;opacity:.8;"' : '' ?>>
                                     <th scope="row">
-                                        <label <?= !$editor_supports ? 'style="color:#6d6d6d;"' : '' ?>><?php _e('Image Size:', 'auto-upload-images'); ?></label>
+                                        <label <?= !$editor_supports ? 'style="color:#6d6d6d;"' : '' ?>><?php _e('Image Size:', 'automatic-upload-images'); ?></label>
                                         <?php if (!$editor_supports) : ?>
-                                        <small style="color:#6d6d6d;"><?php _e('(Inactive)', 'auto-upload-images') ?></small>
+                                        <small style="color:#6d6d6d;"><?php _e('(Inactive)', 'automatic-upload-images') ?></small>
                                         <?php endif; ?>
                                     </th>
                                     <td>
-                                        <label for="max_width"><?php _e('Max Width', 'auto-upload-images'); ?></label>
+                                        <label for="max_width"><?php _e('Max Width', 'automatic-upload-images'); ?></label>
                                         <input name="max_width" type="number" step="5" min="0" id="max_width" placeholder="600" class="small-text" value="<?php echo esc_html(self::getOption('max_width')); ?>" <?php echo !$editor_supports ? 'disabled' : '' ?>>
-                                        <label for="max_height"><?php _e('Max Height', 'auto-upload-images'); ?></label>
+                                        <label for="max_height"><?php _e('Max Height', 'automatic-upload-images'); ?></label>
                                         <input name="max_height" type="number" step="5" min="0" id="max_height" placeholder="400" class="small-text" value="<?php echo esc_html(self::getOption('max_height')); ?>" <?php echo !$editor_supports ? 'disabled' : '' ?>>
-                                        <p class="description"><?php _e('You can choose max width and height for images uploaded by this plugin on your site. If you leave empty each one of fields by default use the original size of the image.', 'auto-upload-images'); ?></p>
+                                        <p class="description"><?php _e('You can choose max width and height for images uploaded by this plugin on your site. If you leave empty each one of fields by default use the original size of the image.', 'automatic-upload-images'); ?></p>
                                         <?php if (!$editor_supports) : ?>
-                                        <p style="color:#535353;font-weight: bold;"><?php _e('To activate this feature please enable Gd or Imagick extensions of PHP.', 'auto-upload-images') ?></p>
+                                        <p style="color:#535353;font-weight: bold;"><?php _e('To activate this feature please enable Gd or Imagick extensions of PHP.', 'automatic-upload-images') ?></p>
                                         <?php endif; ?>
                                     </td>
                                 </tr>
@@ -89,7 +89,7 @@
                             <tr valign="top">
                                 <th scope="row">
                                     <label for="exclude_post_types">
-                                        <?php _e('Exclude Post Types:', 'auto-upload-images'); ?>
+                                        <?php _e('Exclude Post Types:', 'automatic-upload-images'); ?>
                                     </label>
                                 </th>
                                 <td>
@@ -102,32 +102,59 @@
                                             </label>
                                         <?php endforeach; ?>
                                     </p>
-                                    <p class="description"><?php _e('Select the Post Types that you want exclude from automatic uploading', 'auto-upload-images'); ?></p>
+                                    <p class="description"><?php _e('Select the Post Types that you want exclude from automatic uploading', 'automatic-upload-images'); ?></p>
                                 </td>
                             </tr>
                             <tr valign="top">
                                 <th scope="row">
                                     <label for="exclude_urls">
-                                        <?php _e('Exclude Domains:', 'auto-upload-images'); ?>
+                                        <?php _e('Exclude Domains:', 'automatic-upload-images'); ?>
                                     </label>
                                 </th>
                                 <td>
-                                    <p><?php _e('Enter the domains you wish to be excluded from uploading images: (One domain per line)', 'auto-upload-images'); ?></p>
+                                    <p><?php _e('Enter the domains you wish to be excluded from uploading images: (One domain per line)', 'automatic-upload-images'); ?></p>
                                     <p><textarea name="exclude_urls" rows="10" cols="50" id="exclude_urls" class="large-text code" placeholder="https://iranimij.com"><?php echo self::getOption('exclude_urls'); ?></textarea></p>
                                 </td>
                             </tr>
                         </table>
                         <p class="submit">
                             <?php submit_button(null, 'primary', 'submit', false); ?>
-                            <?php submit_button(__('Reset Options', 'auto-upload-images'), 'small', 'reset', false, array(
-                                'onclick' => 'return confirm("'. __('Are you sure to reset all options to defaults?', 'auto-upload-images') .'");'
+                            <?php submit_button(__('Reset Options', 'automatic-upload-images'), 'small', 'reset', false, array(
+                                'onclick' => 'return confirm("'. __('Are you sure to reset all options to defaults?', 'automatic-upload-images') .'");'
                             )) ?>
                         </p>
                     </form>
                 </div>
             </div>
             <div id="postbox-container-1" class="postbox-container">
-
+                <div class="postbox">
+                    <h2 class="hndle ui-sortable-handle"><strong><?php _e('Information', 'automatic-upload-images'); ?></strong></h2>
+                    <div class="inside">
+                        <div class="main">
+                            <ul>
+                                <li class="dashicons-before dashicons-flag" style="color: #82878c">
+                                    <a href="https://github.com/iranimij/automatic-upload-images/issues/new" style="text-decoration: none" target="_blank"><?php _e('Report Bug and Issues', 'automatic-upload-images'); ?></a>
+                                </li>
+                                <li class="dashicons-before dashicons-admin-plugins" style="color: #82878c">
+                                    <a href="https://github.com/iranimij/automatic-upload-images" style="text-decoration: none" target="_blank"><?php _e('Github Repository', 'automatic-upload-images'); ?></a>
+                                </li>
+                                <li class="dashicons-before dashicons-wordpress" style="color: #82878c">
+                                    <a href="https://wordpress.org/plugins/automatic-upload-images/" style="text-decoration: none" target="_blank"><?php _e('Plugin Page in WP', 'automatic-upload-images'); ?></a>
+                                </li>
+                                <li class="dashicons-before dashicons-star-filled" style="color: #82878c">
+                                    <a href=https://wordpress.org/plugins/automatic-upload-images/reviews/?rate=5#new-post" style="text-decoration: none" target="_blank">
+                                        <?php _e('Rate to this plugin', 'automatic-upload-images'); ?>
+                                    </a>
+                                </li>
+                                <li class="dashicons-before dashicons-twitter" style="color: #82878c">
+                                    <a href="https://twitter.com/imanheydari13" title="Iman Heydari" style="text-decoration: none" target="_blank"><?php _e('Twitter of Plugin Developer', 'automatic-upload-images'); ?></a>
+                                </li>
+                            </ul>
+                            <hr>
+                            <p><?php _e('If you are a wp developer and like to contribute to this plugin development or you are a translator and want to have this plugin in your language you can start from <a href="https://github.com/airani/wp-auto-upload" target="_blank">plugin Github repository</a>.', 'automatic-upload-images') ?></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <br class="clear">
