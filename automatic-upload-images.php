e2dd0504ca69b20f65c2119e4c60428865488dd0
iranimij <iranimij@gmail.com>
2021-03-02 17:41:51 +0330

    Fix some problems.

--- automatic-upload-images.php
+++ automatic-upload-images.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       automatic upload images
- * Plugin URI:        http://www.hamyarwoo.com
+ * Plugin URI:        http://www.iranimij.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.2.1
  * Author:            iman heydari
